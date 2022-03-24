f83157e76dbd218e870df9c63e9b4e33f2126df2
AndrewCodes98 <93962809+AndrewCodes98@users.noreply.github.com>
2022-03-24 01:57:26 -0700

    Update LED_Board.ino

--- LED_Board.ino
+++ LED_Board.ino
@@ -13,7 +13,20 @@
  *    -Ratio of d_LED to C = 0.01667m / 0.16m = 0.104
  *    -Therefore, time between two consecutive LEDs lighting up = t_LED = 0.104 * t_period
  *    
- */
+ * Common Errors While Running Program:
+ *    -avrdude: ser_open(): can't open device "/dev/cu.usbmodem11101": No such file or directory
+ *        -Go to Tools -> Board: Select Board
+ *                     -> Processor: Select Processor
+ *                     -> Port: Select Port
+ *    -avrdude: stk500_getsync(): not in sync: resp=0x00
+ *        -Restart Arduino IDE
+ *        
+ *        
+ * Future Plans:
+ *    -Determine direction of movement
+ *    -Alternative Bluetooth Option allowing RGB value commands
+ *    -PONG Game
+ */ 
 
  
 ////////////////////////////////////////////////////////////////////
@@ -21,24 +34,29 @@
 ////////////////////////////////////////////////////////////////////
 #include <FastLED.h> //Library used to address WS2812 LEDs
 
-#define HALL_PIN 2    //Pin connected to Hall Sensor Data Input
-#define LEDS_PIN 8    //Pin connected to LED data input
-#define NUM_LEDS 59   //Number of LEDS per strip on board
+const byte HALL_PIN = 2;    //Pin connected to Hall Sensor Data Input
+const byte LEDS_PIN = 8;    //Pin connected to LED data input
+const byte NUM_LEDS = 59;   //Number of LEDS per strip on board
 
 CRGB leds[NUM_LEDS];  // Initialize array of LEDs
 
-//Time measurement variables in microseconds
-volatile unsigned long period = 10000; //Will hold 
-volatile unsigned long lastMeasurement;
 
-const float ratio = 0.104;
-const unsigned short int readingsPerRevolution = 1;
+//Time measurement variables
+volatile unsigned long period = 10;       // Measured in Milliseconds
+volatile unsigned long lastMeasurement;   // Measured in Milliseconds
+volatile unsigned long currentTime;       // Measured in Milliseconds
+
+const float calibration = 0.082; // Calibration to match speed
+const unsigned short int readingsPerRevolution = 1; //Number of magnets on a wheel
 
 unsigned short int iterations = 0; //Counts the number of cycles copmleted in the duration of LEDS_APART
 unsigned short int color = 0; // Iterates between each color
 
+
+//Function Declarations
 void Pulse_Found();
 void LED_Speed_Match();
+void Ramping_LED_Ends();
 
 ////////////////////////////////////////////////////////////////////
 //END OF DO NOT EDIT BLOCK
@@ -53,14 +71,11 @@ void LED_Speed_Match();
 //YOU MAY MODIFY THE FOLOWWING ITEMS
 ////////////////////////////////////////////////////////////////////
 
-//Specify distance between lit LEDs. Every Nth LED will be lit.
-#define LEDS_APART 8  
+#define LEDS_APART 8  //Specify distance between lit LEDs. Every Nth LED will be lit.
+#define colors 3      //Specify the number of colors to use.
+const byte maxBrightness = 100;  //255 max
 
-//Specify the number of colors to use.
-#define colors 3 
-
-//Specify RGB color values for each color
-CRGB colorArr[colors] = {CRGB(100,0,0), CRGB(0,100,0), CRGB(0,0,100)};
+CRGB colorArr[colors] = {CRGB(maxBrightness,0,0), CRGB(0,maxBrightness,0), CRGB(0,0,maxBrightness)}; //Specify RGB color values for each color
 
 ////////////////////////////////////////////////////////////////////
 //END OF EDIT BLOCK
@@ -88,7 +103,7 @@ void setup(){
   FastLED.clear();
   FastLED.show();
   
-  delay(100); //Delays 100ms to ensure micors() is positive and running smoothly
+  delay(100); //Delays 100ms to ensure millis() is positive and running smoothly.
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -96,11 +111,20 @@ void setup(){
 ////////////////////////////////////////////////////////////////////
 
 
+
+
+
+
+
 void loop(){
   LED_Speed_Match();
 
-  
+
 }
+
+
+
+
 
 
 void LED_Speed_Match(){
@@ -116,22 +140,71 @@ void LED_Speed_Match(){
   }else
     leds[0] = 0;
 
-  //Reset iterations to avoid overflow
-  ++iterations %= LEDS_APART;
+  ++iterations %= LEDS_APART;  //Reset iterations to avoid overflow
+
+
+  Ramping_LED_Ends();
 
   FastLED.show();  
 
-  delay(ratio * readingsPerRevolution * period / 1000);
+
+
+
+  // Wait for LEDs to shine before moving to next set of LEDs to shine
+  currentTime = millis();
+  while(millis() < currentTime + calibration * readingsPerRevolution * period){
+    // We want to use millis() and a while loop rather than delay()
+    // since delay() does not allow for interupts.
+  }
+
+
   
   // If board stops moving, stop lights where they are
-  if(micros() - lastMeasurement < 2000000){
-    for(int i = 0; i < NUM_LEDS; i++){ // Loops through all LEDs
-      period
+  if(millis() - lastMeasurement > 700){  //If no magnet detection for 700 milliseconds
+    currentTime = millis();
+    while(lastMeasurement < currentTime){
+      // Wait some more.
     }
   }
 }
 
+
+
+
+
+void Ramping_LED_Ends(){
+/////////////////////////////////////////////////////////////////////////////////
+//WARNING: pow(base,power) MUST BE LESS THAN LOWEST BRIGHTNESS STATED IN colorArr
+/////////////////////////////////////////////////////////////////////////////////
+
+  int base = 2;
+  int power = 5; // N - 1 elements to ramp
+    
+  leds[0].r = leds[0].r / pow(base,power);
+  leds[0].g = leds[0].g / pow(base,power);
+  leds[0].b = leds[0].b / pow(base,power);
+
+  Serial.println(leds[0].r);
+
+  for(int i = 1; i < power; i++){
+
+    // Front of LED Strip
+    leds[i].r = leds[i].r * base;
+    leds[i].g = leds[i].g * base;
+    leds[i].b = leds[i].b * base;
+
+    // End of LED Strip
+    leds[NUM_LEDS-power+i].r = leds[NUM_LEDS-power+i].r / base;
+    leds[NUM_LEDS-power+i].g = leds[NUM_LEDS-power+i].g / base;
+    leds[NUM_LEDS-power+i].b = leds[NUM_LEDS-power+i].b / base;
+  }
+
+  
+}
+
+
+
 void Pulse_Found(){
-  period = micros() - lastMeasurement;
-  lastMeasurement = micros();
+  period = millis() - lastMeasurement;
+  lastMeasurement = millis();
 }
